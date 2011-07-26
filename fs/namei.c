3f215c1323c52786bd6cec2f3fd1cbae71ba52d2
Linus Torvalds <torvalds@linux-foundation.org>
2011-07-25 22:47:03 -0700

    vfs: fix check_acl compile error when CONFIG_FS_POSIX_ACL is not set
    Commit e77819e57f08 ("vfs: move ACL cache lookup into generic code")
    didn't take the FS_POSIX_ACL config variable into account - when that is
    not set, ACL's go away, and the cache helper functions do not exist,
    causing compile errors like
    
      fs/namei.c: In function 'check_acl':
      fs/namei.c:191:10: error: implicit declaration of function 'negative_cached_acl'
      fs/namei.c:196:2: error: implicit declaration of function 'get_cached_acl'
      fs/namei.c:196:6: warning: assignment makes pointer from integer without a cast
      fs/namei.c:212:11: error: implicit declaration of function 'set_cached_acl'
    
    Reported-by: Markus Trippelsdorf <markus@trippelsdorf.de>
    Acked-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/namei.c
+++ fs/namei.c
@@ -178,6 +178,7 @@ EXPORT_SYMBOL(putname);
 
 static int check_acl(struct inode *inode, int mask)
 {
+#ifdef CONFIG_FS_POSIX_ACL
 	struct posix_acl *acl;
 
 	/*
@@ -221,6 +222,7 @@ static int check_acl(struct inode *inode, int mask)
 	        posix_acl_release(acl);
 	        return error;
 	}
+#endif
 
 	return -EAGAIN;
 }
