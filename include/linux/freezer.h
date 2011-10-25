fbdc65580204d4b7667c5e89f9a622578e14799a
Steve French <smfrench@gmail.com>
2011-10-25 10:02:53 -0500

    Fix build break when freezer not configured
    fs/cifs/transport.c: In function 'wait_for_response':
    fs/cifs/transport.c:328: error: implicit declaration of function 'wait_event_freezekillable'
    
    Caused by commit f06ac72e9291 ("cifs, freezer: add
    wait_event_freezekillable and have cifs use it").  In this config,
    CONFIG_FREEZER is not set.
    
    Reviewed-by: Shirish Pargaonkar <shirishp@us.ibm.com>
    CC: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <smfrench@gmail.com>
    (cherry picked from commit cefde91926fd652a1a7c220321540b550e6476ce)

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -190,6 +190,9 @@ static inline void set_freezable(void) {}
 #define wait_event_freezable_timeout(wq, condition, timeout)		\
 		wait_event_interruptible_timeout(wq, condition, timeout)
 
+#define wait_event_freezekillable(wq, condition)		\
+		wait_event_killable(wq, condition)
+
 #endif /* !CONFIG_FREEZER */
 
 #endif	/* FREEZER_H_INCLUDED */
