00366cc09db1687017272f6db766eca31df4b104
Al Viro <viro@zeniv.linux.org.uk>
2014-10-11 23:05:52 -0400

    let path_init() failures treated the same way as subsequent link_path_walk()
    As it is, path_lookupat() and path_mounpoint() might end up leaking struct file
    reference in some cases.
    
    Spotted-by: Eric Biggers <ebiggers3@gmail.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namei.c
+++ fs/namei.c
@@ -1788,7 +1788,7 @@ static int path_lookupat(int dfd, const char *name,
 	err = path_init(dfd, name, flags | LOOKUP_PARENT, nd, &base);
 
 	if (unlikely(err))
-		return err;
+		goto out;
 
 	current->total_link_count = 0;
 	err = link_path_walk(name, nd);
@@ -1816,6 +1816,7 @@ static int path_lookupat(int dfd, const char *name,
 		}
 	}
 
+out:
 	if (base)
 		fput(base);
 
