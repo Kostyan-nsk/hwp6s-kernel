92460e9539e5aca5d974b401f4c9bb00ea92c5a6
Kostyan-nsk <Kostyan-nsk@users.noreply.github.com>
2015-04-01 15:23:30 +0800

    3.0.17

--- drivers/usb/class/cdc-acm.c
+++ drivers/usb/class/cdc-acm.c
@@ -648,17 +648,17 @@ static void acm_port_down(struct acm *acm)
 
 static void acm_tty_hangup(struct tty_struct *tty)
 {
-	struct acm *acm = tty->driver_data;
+	struct acm *acm;
 
-	if (!acm) {
-		return;
-	}
-
-	dev_info(&acm->data->dev, "acm_tty_hangup\n");
+	mutex_lock(&open_mutex);
+	acm = tty->driver_data;
+	if (!acm)
+		goto out;
 
 	tty_port_hangup(&acm->port);
 	acm_port_down(acm);
-	tty->driver_data = NULL;
+out:
+	mutex_unlock(&open_mutex);
 }
 
 static void acm_tty_close(struct tty_struct *tty, struct file *filp)
@@ -1310,6 +1310,8 @@ made_compressed_probe:
 		i = device_create_file(&intf->dev, &dev_attr_wCountryCodes);
 		if (i < 0) {
 			kfree(acm->country_codes);
+			acm->country_codes = NULL;
+			acm->country_code_size = 0;
 			goto skip_countries;
 		}
 
@@ -1318,6 +1320,8 @@ made_compressed_probe:
 		if (i < 0) {
 			device_remove_file(&intf->dev, &dev_attr_wCountryCodes);
 			kfree(acm->country_codes);
+			acm->country_codes = NULL;
+			acm->country_code_size = 0;
 			goto skip_countries;
 		}
 	}
