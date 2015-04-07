d0e2297fee53afb4cebf9d5b4fe58870ae9e1af7
Kostyan-nsk <Kostyan-nsk@users.noreply.github.com>
2015-04-07 13:16:35 +0800

    3.0.62

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -895,12 +895,12 @@ static void dma_cache_maint_page(struct page *page, unsigned long offset,
 	do {
 		size_t len = left;
 		void *vaddr;
+
 		page =pfn_to_page(pfn);
 
 		if (PageHighMem(page)) {
-			if (len + offset > PAGE_SIZE){
+			if (len + offset > PAGE_SIZE)
 				len = PAGE_SIZE - offset;
-			}
 			vaddr = kmap_high_get(page);
 			if (vaddr) {
 				vaddr += offset;
