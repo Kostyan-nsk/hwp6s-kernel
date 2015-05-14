a74200014354c87d80483078a2ee668e8f33f34a
Kostyan-nsk <Kostyan-nsk@users.noreply.github.com>
2015-05-14 14:33:56 +0800

    intelliplug: adjustments

--- arch/arm/hotplug/intelli_plug.c
+++ arch/arm/hotplug/intelli_plug.c
@@ -55,10 +55,10 @@ static struct workqueue_struct *intelliplug_boost_wq;
 unsigned int intelli_plug_active = 0;
 module_param(intelli_plug_active, uint, 0664);
 
-static unsigned int touch_boost_active = 1;
+static unsigned int touch_boost_active = 0;
 module_param(touch_boost_active, uint, 0664);
 
-static unsigned int nr_run_profile_sel = 0;
+static unsigned int nr_run_profile_sel = 1;
 module_param(nr_run_profile_sel, uint, 0664);
 
 //default to something sane rather than zero
