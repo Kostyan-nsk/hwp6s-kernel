c0b51041eb1277a777b62856e842a1604ee31daa
Michael Heltne <you@example.com>
2020-07-26 16:30:57 +0200

    oh god what a mess

--- fs/mount.h
+++ fs/mount.h
@@ -1,18 +1,6 @@
 #include <linux/mount.h>
 
-struct mount {
-	struct list_head mnt_hash;
-	struct mount *mnt_parent;
-	struct dentry *mnt_mountpoint;
-	struct vfsmount mnt;
-};
-
-static inline struct mount *real_mount(struct vfsmount *mnt)
+static inline int mnt_has_parent(struct vfsmount *mnt)
 {
-	return container_of(mnt, struct mount, mnt);
-}
-
-static inline int mnt_has_parent(struct mount *mnt)
-{
-	return &mnt->mnt != mnt->mnt_parent;
+	return mnt != mnt->mnt_parent;
 }
