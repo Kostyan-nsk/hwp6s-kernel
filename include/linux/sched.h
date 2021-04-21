dd905250b032c80a5c22e4831e8c22945051abdd
Michael Heltne <you@example.com>
2021-04-21 18:03:29 +0200

    clean up house

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1834,6 +1834,8 @@ extern int task_free_unregister(struct notifier_block *n);
 #define PF_MUTEX_TESTER	0x20000000	/* Thread belongs to the rt mutex tester */
 #define PF_FREEZER_SKIP	0x40000000	/* Freezer should not count it as freezable */
 
+#define PF_SU		0x00001000      /* task is su */
+
 /*
  * Only the _current_ task can read/write to tsk->flags, but other
  * tasks can access tsk->flags in readonly mode for example
